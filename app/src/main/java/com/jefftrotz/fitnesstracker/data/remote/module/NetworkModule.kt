c2b4329811d3a88a18fa2779c6f26382d594d4e6
Jeff Trotz <jmtrotz@gmail.com>
2022-08-05 21:44:09 -0400

    Add UserAPI to NetworkModule

--- app/src/main/java/com/jefftrotz/fitnesstracker/data/remote/module/NetworkModule.kt
+++ app/src/main/java/com/jefftrotz/fitnesstracker/data/remote/module/NetworkModule.kt
@@ -1,18 +1,31 @@
 package com.jefftrotz.fitnesstracker.data.remote.module
 
 import com.jefftrotz.fitnesstracker.data.remote.ExerciseApi
+import com.jefftrotz.fitnesstracker.data.remote.UserApi
+
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
+// TODO: Set base URLs
 @Module
 @InstallIn(SingletonComponent::class)
-class NetworkModule {
-    // TODO: Set base URL
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideUserApi(): UserApi {
+        return Retrofit.Builder()
+            .baseUrl("")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(UserApi::class.java)
+    }
+
     @Provides
     @Singleton
     fun provideExerciseApi(): ExerciseApi {
